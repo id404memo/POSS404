6398d9cc96a13de40dc5492db519112f68d3ac1a
memo <id404memo@gmail.com>
2026-09-25 15:45:52 -0600

    fix: delete duplicate colors_1 themes_1 duplicate resources

--- app/src/main/java/com/memo/poss404/ui/navigation/Nav_1.kt
+++
@@ -1,9 +0,0 @@
-
-package com.memo.poss404.ui.navigation
-object Routes {
-    const val PRODUCTOS = "productos"
-    const val CLIENTES = "clientes"
-    const val CARRITO = "carrito"
-    const val VENTAS = "ventas"
-    const val AJUSTES = "ajustes"
-}
